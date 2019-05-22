66b3e566fc7bd74d1f7fad634529f0637df1370d
Riad Akrour <akrour@ias.informatik.tu-darmstadt.de>
2019-05-22 17:39:42 +0200

    lin gauss proj

--- gaussian_proj.py
+++ gaussian_proj.py
@@ -27,25 +27,57 @@ def utils_from_chol(chol):
 
 
 def gauss_kl_proj(means, chol, old_means, old_cov, old_prec, old_logdetcov, epsilon):
+    # KL violation
     cov = chol.mm(chol.t())
     m = mean_diff(means, old_means, old_prec)
     r = rot_diff(cov, old_prec)
     e = entropy_diff(chol, old_logdetcov)
-    init_kl = r + m + e
-    eta_cov = 1.
-    eta_mean = 1.
+    init_kl = m + r + e
+    eta_cov = torch.tensor(1.)
+    eta_mean = torch.tensor(1.)
 
     if init_kl > epsilon + 1e-6:
-        eta_cov = epsilon / torch.clamp(r + m + e, 1e-16)
+        eta_cov = epsilon / init_kl
         ncov = (1 - eta_cov) * old_cov + eta_cov * cov
         chol, cov = ncov.cholesky(), ncov
         r, e = rot_diff(cov, old_prec), entropy_diff(chol, old_logdetcov)
 
-    if r + m + e > epsilon + 1e-6:
-        eta_mean = torch.sqrt(torch.clamp(epsilon - e - r, 0) / torch.clamp(m, 1e-6))
+    if m + r + e > epsilon + 1e-6:
+        eta_mean = torch.sqrt(torch.clamp(epsilon - e - r, min=0) / torch.clamp(m, min=1e-6))
         means = (1 - eta_mean) * old_means + eta_mean * means
         m = mean_diff(means, old_means, old_prec)
 
-    final_kl = r + m + e
+    final_kl = m + r + e
+
+    return {'eta_mean': eta_mean, 'eta_cov': eta_cov, 'means': means, 'chol': chol, 'init_kl': init_kl, 'final_kl': final_kl}
+
+
+def lin_gauss_kl_proj(means, chol, intermediate_means, old_means, old_cov, old_prec, old_logdetcov, epsilon):
+    # KL violation
+    cov = chol.mm(chol.t())
+    m = mean_diff(means, old_means, old_prec)
+    r = rot_diff(cov, old_prec)
+    e = entropy_diff(chol, old_logdetcov)
+    init_kl = m + r + e
+    inter_mean_diff = mean_diff(intermediate_means, old_means, old_prec)
+    mm = torch.min(m, inter_mean_diff)
+    eta_cov = torch.tensor(1.)
+    eta_mean = torch.tensor(1.)
+
+    if init_kl > epsilon + 1e-6:
+        eta_cov = (epsilon - mm) / init_kl
+        ncov = (1. - eta_cov) * old_cov + eta_cov * cov
+        chol, cov = ncov.cholesky(), ncov
+        r, e = rot_diff(cov, old_prec), entropy_diff(chol, old_logdetcov)
+
+    if m + r + e > epsilon + 1e-6:
+        a = mean_diff(means, intermediate_means, old_prec)
+        b = cross_mul(means - intermediate_means, old_prec, intermediate_means - old_means)
+        c = epsilon - inter_mean_diff - e - r
+        eta_mean = (-b + torch.sqrt(torch.clamp(b * b + a * c, min=1e-16))) / torch.clamp(a, min=1e-6)
+        means = (1. - eta_mean) * old_means + eta_mean * means
+        m = mean_diff(means, old_means, old_prec)
+
+    final_kl = m + r + e
 
     return {'eta_mean': eta_mean, 'eta_cov': eta_cov, 'means': means, 'chol': chol, 'init_kl': init_kl, 'final_kl': final_kl}
