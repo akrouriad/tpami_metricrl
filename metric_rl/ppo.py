Minor fix in mean reward computation
- now computing the mean over the appropriate number of episodes
- Still not considering correct episodes, as the first step in the
dataset could potentially be a non starting state

metric_rl/ppo.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 from mushroom.algorithms.agent import Agent
 from mushroom.approximators import Regressor
 from mushroom.approximators.parametric import PyTorchApproximator
-from mushroom.utils.dataset import parse_dataset
+from mushroom.utils.dataset import parse_dataset, compute_J
 from mushroom.utils.minibatches import minibatch_generator
 
 from .policies import PyTorchPolicy, GaussianPolicy
@@ -101,7 +101,7 @@ class PPO(Agent):
         logging_ent = self._policy_torch.entropy()
         new_pol_dist = self._policy_torch.distribution(obs)
         logging_kl = torch.mean(torch.distributions.kl.kl_divergence(new_pol_dist, old_pol_dist))
-        avg_rwd = np.sum(r) / np.sum(last)
+        avg_rwd = np.mean(compute_J(dataset))
         tqdm.write("Iterations Results:\n\trewards {} vf_loss {}\n\tentropy {}  kl {}".format(
             avg_rwd, logging_verr, logging_ent, logging_kl))
         tqdm.write('--------------------------------------------------------------------------------------------------')

run_gym_deep.py
@@ -13,7 +13,7 @@ from metric_rl.logger import generate_log_folder, save_parameters, Logger
 
 def experiment(alg, env_id, horizon, gamma, n_epochs, n_steps, n_steps_per_fit, n_episodes_test, seed, params,
                log_name=None):
-    print('Metric RL')
+    print(alg.__name__)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.set_num_threads(1)
@@ -89,8 +89,8 @@ if __name__ == '__main__':
                        )
 
     algs_params = [
-        (TRPO, 'trpo', trpo_params),
-        #(PPO, 'ppo', ppo_params)
+        #(TRPO, 'trpo', trpo_params),
+        (PPO, 'ppo', ppo_params)
      ]
 
     # Bipedal Walker