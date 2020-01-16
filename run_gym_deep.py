54f61d2ae713954458af18278554e1f4845e3389
boris-il-forte <boris.ilpossente@hotmail.it>
2020-01-16 16:35:36 +0100

    Fixed deep experiments scripts
    - updated deep experiments parameters
    - created runner for cluster experiments

--- run_gym_deep.py
+++ run_gym_deep.py
@@ -15,9 +15,9 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 
-def experiment(alg, env_id, horizon, gamma, n_epochs, n_steps, n_steps_per_fit, n_episodes_test,
-               alg_params, critic_params, policy_params, seed, log_name=None):
-    print(alg.__name__)
+def experiment(alg_name, env_id, horizon, gamma, n_epochs, n_steps, n_steps_per_fit, n_episodes_test,
+               seed, n_models_v=1, log_name=None, visualize=False):
+    print(alg_name)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.set_num_threads(1)
@@ -29,25 +29,40 @@ def experiment(alg, env_id, horizon, gamma, n_epochs, n_steps, n_steps_per_fit, 
     # Set environment seed
     mdp.env.seed(seed)
 
+    # Set parameters
+    policy_params = dict(std_0=1.,
+                         size_list=[64, 64],
+                         use_cuda=False)
+
     critic_params = dict(network=MLP,
                          loss=F.mse_loss,
                          input_shape=mdp.info.observation_space.shape,
                          output_shape=(1,),
-                         **critic_params)
+                         optimizer={'class': optim.Adam,
+                                    'params': {'lr': 3e-4}},
+                         n_models=n_models_v,
+                         size_list=[64, 64])
+
+    alg, alg_params = get_alg_and_parameters(alg_name)
 
     policy = GaussianTorchPolicy(MLP,
                                  mdp.info.observation_space.shape,
                                  mdp.info.action_space.shape,
                                  **policy_params)
 
-    agent = alg(mdp.info, policy, critic_params, **alg_params)
+    agent = alg(mdp.info, policy, critic_params=critic_params, **alg_params)
+
+    # Save alg params
+    save_parameters(log_name, dict(alg_params=alg_params))
+
+    # Run learning
     core = Core(agent, mdp)
 
     J_list = list()
     R_list = list()
     E_list = list()
 
-    for it in trange(n_epochs):
+    for it in range(n_epochs):
         core.learn(n_steps=n_steps, n_steps_per_fit=n_steps_per_fit)
         dataset = core.evaluate(n_episodes=n_episodes_test, render=False)
 
@@ -59,18 +74,49 @@ def experiment(alg, env_id, horizon, gamma, n_epochs, n_steps, n_steps_per_fit, 
         R_list.append(R)
         E_list.append(E)
 
-        logger.save(network=agent.policy, J=J_list, R=R_list, E=E_list)
+        logger.save(J=J_list, R=R_list, E=E_list)
 
         tqdm.write('END OF EPOCH ' + str(it))
         tqdm.write('J: {}, R: {}, entropy: {}'.format(J, R, E))
         tqdm.write('##################################################################################################')
 
-    print('Press a button to visualize')
-    input()
-    core.evaluate(n_episodes=5, render=True)
+    if visualize:
+        print('Press a button to visualize')
+        input()
+        core.evaluate(n_episodes=5, render=True)
+
+
+def get_alg_and_parameters(alg_name):
+    if alg_name is 'PPO':
+        alg_params = dict(actor_optimizer={'class': optim.Adam,
+                                       'params': {'lr': 3e-4}},
+                          n_epochs_policy=10,
+                          batch_size=64,
+                          eps_ppo=.2,
+                          lam=.95,
+                          quiet=True)
+
+        return PPO, alg_params
+
+    elif alg_name is 'TRPO':
+        alg_params = dict(ent_coeff=0.0,
+                          max_kl=.01,
+                          lam=.95,
+                          n_epochs_line_search=10,
+                          n_epochs_cg=10,
+                          cg_damping=1e-2,
+                          cg_residual_tol=1e-10,
+                          quiet=True)
+
+        return TRPO, alg_params
+
+    else:
+        raise RuntimeError
 
 
 if __name__ == '__main__':
+    # Algs
+    algs = ['PPO', 'TRPO']
 
     # BipedalWalker
     env_id = 'BipedalWalker-v2'
@@ -82,45 +128,8 @@ if __name__ == '__main__':
     # horizon = 1000
     # gamma = .99
 
-    # Policy Parameters
-    policy_params = dict(std_0=1.,
-                         size_list=[64, 64],
-                         use_cuda=False)
-
-    # Critic Parameters
-    n_models_v = 1
-    critic_params = dict(optimizer={'class': optim.Adam,
-                                    'params': {'lr': 3e-4}},
-                         n_models=n_models_v,
-                         size_list=[64, 64])
-
-    # Alg Parameters
-    ppo_alg_params = dict(lr_p=3e-4,
-                          n_epochs_v=10,
-                          n_epochs_policy=10,
-                          batch_size=64,
-                          eps_ppo=.2,
-                          lam=.95,
-                          quiet=False)
-
-    trpo_alg_params = dict(ent_coeff=0.0,
-                           max_kl=.01,
-                           lam=.95,
-                           n_epochs_line_search=10,
-                           n_epochs_cg=10,
-                           cg_damping=1e-2,
-                           cg_residual_tol=1e-10,
-                           quiet=False)
-
-    algs_params = [
-        (TRPO, 'trpo', trpo_alg_params),
-        (PPO, 'ppo', ppo_alg_params)
-     ]
-
-    for alg, alg_name, alg_params in algs_params:
-        log_name = generate_log_folder(env_id, alg_name, str(n_models_v), True)
-        save_parameters(log_name, dict(alg_params=alg_params, critic_params=critic_params, policy_params=policy_params))
-        experiment(alg=alg, env_id=env_id, horizon=horizon, gamma=gamma,
-                   n_epochs=100, n_steps=30000, n_steps_per_fit=3000, n_episodes_test=10,
-                   alg_params=alg_params, critic_params=critic_params, policy_params=policy_params,
-                   seed=0, log_name=log_name)
+    for alg_name in algs:
+        log_name = generate_log_folder(env_id, alg_name, timestamp=True)
+        experiment(alg_name=alg_name, env_id=env_id, horizon=horizon, gamma=gamma,
+                   n_epochs=2, n_steps=30000, n_steps_per_fit=3000, n_episodes_test=10,
+                   seed=0, log_name=log_name, visualize=True)
