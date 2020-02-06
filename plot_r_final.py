0f5edb0ef5ee3722964628eaa893b9e11e69e254
boris-il-forte <boris.ilpossente@hotmail.it>
2020-02-06 10:44:41 +0100

    Updated scripts and results for small envs

--- plot_r_final.py
+++ plot_r_final.py
@@ -54,7 +54,7 @@ def create_figure(res_folder, subfolder, name, env, all_data, alg_labels, use_me
     plt.clf()
 
 
-def load_data_base(res_folder, env, subfolder, nb_runs, entropy=True):
+def load_data_base(res_folder, env, subfolder, nb_runs, nb_epochs, entropy=True):
     all_r_a = []
     all_j_a = []
     all_entropy_a = []
@@ -73,11 +73,11 @@ def load_data_base(res_folder, env, subfolder, nb_runs, entropy=True):
             R = np.load(r_name)
             if entropy:
                 E = np.load(e_name)
-            if R.shape[0] != 1001:
+            if R.shape[0] != nb_epochs + 1:
                 print(r_name, 'wrong shape')
                 print(R.shape, J.shape)
-                J = J[:1001]
-                R = R[:1001]
+                J = J[:nb_epochs+1]
+                R = R[:nb_epochs+1]
             all_j_a.append(J)
             all_r_a.append(R)
             if entropy:
@@ -91,7 +91,7 @@ def load_data_base(res_folder, env, subfolder, nb_runs, entropy=True):
         return all_j_a, all_r_a
 
 
-def load_data_acost(res_folder, env, alg_name, n_clusterss, a_cost_scales, nb_runs):
+def load_data_acost(res_folder, env, alg_name, n_clusterss, a_cost_scales, nb_runs, nb_epochs):
     all_j = []
     all_r = []
     all_entropy = []
@@ -103,7 +103,7 @@ def load_data_acost(res_folder, env, alg_name, n_clusterss, a_cost_scales, nb_ru
             alg_label = 'c' + str(n_clusters) + 'a' + str(a_cost_scale)
             subfolder = alg_name + '_' + alg_label
 
-            all_j_a, all_r_a, all_entropy_a = load_data_base(res_folder, env, subfolder, nb_runs)
+            all_j_a, all_r_a, all_entropy_a = load_data_base(res_folder, env, subfolder, nb_runs, nb_epochs)
 
             all_j.append(all_j_a)
             all_r.append(all_r_a)
@@ -113,7 +113,7 @@ def load_data_acost(res_folder, env, alg_name, n_clusterss, a_cost_scales, nb_ru
     return all_j, all_r, all_entropy, alg_labels
 
 
-def load_data_entropy(res_folder, env, alg_name, n_clusterss, nb_runs):
+def load_data_entropy(res_folder, env, alg_name, n_clusterss, nb_runs, nb_epochs):
     all_j = []
     all_r = []
     all_entropy = []
@@ -123,7 +123,7 @@ def load_data_entropy(res_folder, env, alg_name, n_clusterss, nb_runs):
         alg_label = 'c' + str(n_clusters)
         subfolder = alg_name + '_' + alg_label
 
-        all_j_a, all_r_a, all_entropy_a = load_data_base(res_folder, env, subfolder, nb_runs)
+        all_j_a, all_r_a, all_entropy_a = load_data_base(res_folder, env, subfolder, nb_runs, nb_epochs)
 
         all_j.append(all_j_a)
         all_r.append(all_r_a)
@@ -133,14 +133,14 @@ def load_data_entropy(res_folder, env, alg_name, n_clusterss, nb_runs):
     return all_j, all_r, all_entropy, alg_labels
 
 
-def load_data_baselines(res_folder, env, alg_names, nb_runs, entropy=False):
+def load_data_baselines(res_folder, env, alg_names, nb_runs, nb_epochs, entropy=False):
     all_j = []
     all_r = []
 
     for alg_name in alg_names:
         subfolder = alg_name
 
-        all_j_a, all_r_a = load_data_base(res_folder, env, subfolder, nb_runs, entropy=entropy)
+        all_j_a, all_r_a = load_data_base(res_folder, env, subfolder, nb_runs, nb_epochs, entropy=entropy)
 
         all_j.append(all_j_a)
         all_r.append(all_r_a)
@@ -148,7 +148,7 @@ def load_data_baselines(res_folder, env, alg_names, nb_runs, entropy=False):
     return all_j, all_r
 
 
-def load_data_fixedtemp(res_folder, env, alg_name, n_clusterss, nb_runs, clus_sels, clus_dels, temps):
+def load_data_fixedtemp(res_folder, env, alg_name, n_clusterss, nb_runs, nb_epochs, clus_sels, clus_dels, temps):
     all_j = []
     all_r = []
     all_entropy = []
@@ -161,7 +161,7 @@ def load_data_fixedtemp(res_folder, env, alg_name, n_clusterss, nb_runs, clus_se
                     alg_label = 'c' + str(n_clusters) + 'h' + clus_sel + 'd' + str(clus_del) + 't' + str(temp) + 'snone'
                     subfolder = alg_name + '_' + alg_label
                     print(subfolder)
-                    all_j_a, all_r_a, all_entropy_a = load_data_base(res_folder, env, subfolder, nb_runs)
+                    all_j_a, all_r_a, all_entropy_a = load_data_base(res_folder, env, subfolder, nb_runs, nb_epochs)
 
                     all_j.append(all_j_a)
                     all_r.append(all_r_a)
@@ -173,17 +173,27 @@ def load_data_fixedtemp(res_folder, env, alg_name, n_clusterss, nb_runs, clus_se
 
 if __name__ == '__main__':
 
-    plot_mushroom = False
-    # xp_name = 'entropy'
-    xp_name = 'final_medium'
+    plot_baselines = False
+
+    xp_name = 'final_small2'
+    envs = ['BipedalWalker-v2', 'InvertedPendulumBulletEnv-v0', 'Pendulum-v0', 'InvertedPendulumSwingupBulletEnv-v0']
+    temps = [1., 1., 1., 1.]
+    n_clusterss = [5, 10, 20]
+    nb_epochs = 500
+    alg_labels = ['Metric-5', 'Metric-10', 'Metric-20']
+
+    # xp_name = 'final_medium'
+    # envs = ['HopperBulletEnv-v0', 'Walker2DBulletEnv-v0', 'HalfCheetahBulletEnv-v0', 'AntBulletEnv-v0']
+    # temps = [1., 1., .33, .33]
+    # n_clusterss = [10, 20, 40]
+    # nb_epochs = 1000
+    # alg_labels = ['Metric-10', 'Metric-20', 'Metric-40', 'PPO', 'TRPO (MLP)', 'TRPO (Linear)']
+
     res_folder = './Results/'
     exp_folder = os.path.join(res_folder, xp_name)
     baseline_folder = os.path.join(res_folder, 'baselines')
-    baseline_mushroom_folder = os.path.join(res_folder, 'baselines')
-    envs = ['HopperBulletEnv-v0', 'Walker2DBulletEnv-v0', 'HalfCheetahBulletEnv-v0', 'AntBulletEnv-v0']
-    temps = [1., 1., .33, .33]
+
     nb_runs = 25
-    n_clusterss = [10, 20, 40]
     # clus_sels = ['old_covr_yetnew']
     clus_sels = ['covr_exp']
     clus_dels = [True]
@@ -192,15 +202,17 @@ if __name__ == '__main__':
     baselines_algs = ['ppo2', 'trpo_mpi', 'trpo_linear']
     baselines_mushroom_algs = ['PPO', 'TRPO']
 
-    alg_labels = ['Metric-10', 'Metric-20', 'Metric-40', 'PPO', 'TRPO (MLP)', 'TRPO (Linear)']
+
 
     # Creating parameters tables
     count = 0
     for temp, env in zip(temps, envs):
-        all_j, all_r, all_e, _ = load_data_fixedtemp(exp_folder, env, alg_name, n_clusterss, nb_runs, clus_sels, clus_dels, [temp])
-        b_j, b_r = load_data_baselines(baseline_folder, env, baselines_algs, nb_runs)
-        all_j += b_j
-        all_r += b_r
+        all_j, all_r, all_e, _ = load_data_fixedtemp(exp_folder, env, alg_name, n_clusterss, nb_runs, nb_epochs, clus_sels, clus_dels, [temp])
+
+        if plot_baselines:
+            b_j, b_r = load_data_baselines(baseline_folder, env, baselines_algs, nb_runs)
+            all_j += b_j
+            all_r += b_r
 
         create_figure(res_folder, xp_name, 'R', env, all_r, alg_labels,
                       legend=(count + 1 == len(envs)))
